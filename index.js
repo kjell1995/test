e709d4f927edeb23396e0c527850e6a8ae116b5c
Kjell Deboysere <Kjell Deboysere>
2016-10-19 10:23:27 +0200

    added 500 page

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use((req, res)=>{
 //custom 5000 page
 app.use((err,req,res,next)=>{
     console.error(err.stack);
-    res.type("text/plain");
     res.status(500);
-    res.send("500 - Server Error");
+    res.render("500");
+    
 });
 
 app.listen(app.get("port"),() =>{
